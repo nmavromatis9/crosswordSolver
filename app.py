5644dd71a6348ba5f1ff568717bbe653b5b49896
nmavromatis9 <52474885+nmavromatis9@users.noreply.github.com>
2023-05-24 16:05:08 -0400

    test

--- app.py
+++ app.py
@@ -30,14 +30,8 @@ def index():
 
 @app.route('/results', methods = ['POST', 'GET'])
 def results():
-    if request.method == 'POST':
-        result = request.form
-        l=accessDict(result['crossword'])
-        if (len(l)==0):
-            return render_template("none.html", path=url_for('index'))
-        return render_template("result.html", form=l, path=url_for('index'))
-    else:
-        return render_template("none.html", path=url_for('index'))
+    l=accessDict("chic***")
+    return render_template("result.html", form=l, path=url_for('index'))
 
 @app.route('/about', methods=['GET', 'POST'])
 def about():   
